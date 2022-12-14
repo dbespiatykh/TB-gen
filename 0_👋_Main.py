021f0d101ec6c077b13660fe33181ef58bcd22b8
dbespiatykh <d.bespiatykh@gmail.com>
2022-12-14 18:21:23 +0300

    Path fix

--- 0_👋_Main.py
+++ 0_👋_Main.py
@@ -68,7 +68,7 @@ html, body, [class*="css"]  {
 
 
 # Read .csv from a URL
-dataset_url = "data/tb_data.csv"
+dataset_url = "tb_data.csv"
 
 
 @st.experimental_memo
@@ -99,7 +99,7 @@ dr_snps = (
 # Load Country Shapes file
 # country_url = 'https://raw.githubusercontent.com/python-visualization/folium/master/examples/data'
 # country_shapes = f'{country_url}/world-countries.json'
-country_shapes = "data/world_countries.json"
+country_shapes = "world_countries.json"
 country = geopandas.read_file(country_shapes)
 
 # Calculate number of samples per country
@@ -119,7 +119,7 @@ cnt_samples_poly = (
 )
 
 # Load configuration file for kepler
-with open("data/config.pkl", "rb") as f:
+with open("config.pkl", "rb") as f:
     config = pickle.load(f)
 
 ####################################################################################################################
