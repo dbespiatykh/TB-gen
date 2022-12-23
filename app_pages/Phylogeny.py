2c9f54fedad0b4bc9c2cf6549ddb7c8c6b9ea2f8
dbespiatykh <d.bespiatykh@gmail.com>
2022-12-23 03:05:29 +0300

    Add l4,5-Animal html trees

--- app_pages/Phylogeny.py
+++ app_pages/Phylogeny.py
@@ -14,32 +14,6 @@ st.title("_Mycobacterium tuberculosis_ phylogeny")
 st.markdown("---")
 
 
-def show_svg(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<iframe src="data:image/svg+xml;base64,{base64_pdf}" width="1100" height="900" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-def displayPDF(file):
-    # Opening file from file path
-    with open(file, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-
-    # Embedding PDF in HTML
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></embed>'
-
-    # Displaying File
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-def show_pdf(file_path):
-    with open(file_path, "rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-    st.markdown(pdf_display, unsafe_allow_html=True)
-
-
 def show_lineage1_tree():
     HtmlTree = open("./data/trees/lineage.1.tree.html", "r", encoding="utf-8")
     source_code = HtmlTree.read()
@@ -58,9 +32,16 @@ def show_lineage3_tree():
     components.html(source_code, height=1000, width=1800, scrolling=True)
 
 
-# HtmlTree = open("./data/tree-plot.html", "r", encoding="utf-8")
-# source_code = HtmlTree.read()
-# components.html(source_code, height=1500, scrolling=True)
+def show_lineage4_tree():
+    HtmlTree = open("./data/trees/lineage.4.tree.html", "r", encoding="utf-8")
+    source_code = HtmlTree.read()
+    components.html(source_code, height=1000, width=1800, scrolling=True)
+
+
+def show_lineage5_tree():
+    HtmlTree = open("./data/trees/lineage.5.Animal.tree.html", "r", encoding="utf-8")
+    source_code = HtmlTree.read()
+    components.html(source_code, height=1000, width=1600, scrolling=True)
 
 
 tab1, tab2, tab3, tab4, tab5 = st.tabs(
@@ -69,7 +50,7 @@ tab1, tab2, tab3, tab4, tab5 = st.tabs(
         "Lineage 2",
         "Lineage 3",
         "Lineage 4",
-        "MTBC",
+        "Lineage 5-Animal",
     ]
 )
 
@@ -83,13 +64,21 @@ with tab3:
     show_lineage3_tree()
 
 with tab4:
-    show_svg("./data/trees/lineage4_tree.svg")
+    show_lineage4_tree()
 
 with tab5:
-    show_svg("./data/trees/mtbc_tree.svg")
+    show_lineage5_tree()
 
-# # TREE TEST
 
-# HtmlTree = open("./data/tree-plot.html", "r", encoding="utf-8")
-# source_code = HtmlTree.read()
-# components.html(source_code, height=1500, scrolling=True)
+# def show_svg(file_path):
+#     with open(file_path, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+#     pdf_display = f'<iframe src="data:image/svg+xml;base64,{base64_pdf}" width="1100" height="900" type="application/pdf"></iframe>'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
+
+
+# def show_pdf(file_path):
+#     with open(file_path, "rb") as f:
+#         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
+#     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
+#     st.markdown(pdf_display, unsafe_allow_html=True)
