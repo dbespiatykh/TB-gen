49ac13e65f86e6c3a203c83d2ce2d250dc8fb7e9
dbespiatykh <d.bespiatykh@gmail.com>
2023-01-10 03:23:26 +0300

    Delete commented out lines

--- app_pages/Phylogeny.py
+++ app_pages/Phylogeny.py
@@ -69,17 +69,3 @@ with tab4:
 
 with tab5:
     show_lineage5_tree()
-
-
-# def show_svg(file_path):
-#     with open(file_path, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-#     pdf_display = f'<iframe src="data:image/svg+xml;base64,{base64_pdf}" width="1100" height="900" type="application/pdf"></iframe>'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
-
-
-# def show_pdf(file_path):
-#     with open(file_path, "rb") as f:
-#         base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-#     pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">'
-#     st.markdown(pdf_display, unsafe_allow_html=True)
