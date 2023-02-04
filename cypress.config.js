61d9ef522d9001201c9cfa632d41311d86c6b549
dbespiatykh <d.bespiatykh@gmail.com>
2023-02-04 03:47:19 +0300

    Add retries

--- cypress.config.js
+++ cypress.config.js
@@ -6,5 +6,13 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {},
     specPattern: "cypress/e2e/**/*.{js,jsx,ts,tsx}",
     supportFile: false,
+    retries: {
+      // Configure retry attempts for `cypress run`
+      // Default is 0
+      runMode: 5,
+      // Configure retry attempts for `cypress open`
+      // Default is 0
+      openMode: 5,
+    },
   },
 });
